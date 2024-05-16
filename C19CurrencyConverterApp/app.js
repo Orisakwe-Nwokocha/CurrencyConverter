6eb08f650a7fc2efb2eed8b8b449713f1d8ff408
Orisha <orisakwenwokocha1@gmail.com>
2024-05-16 18:07:39 +0100

    completed currency exchange app

--- C19CurrencyConverterApp/app.js
+++ C19CurrencyConverterApp/app.js
@@ -8,23 +8,21 @@ const converter = document.forms["input-form"];
 converter.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const sourceCurrency = document.getElementsByName("source-currency")[0].value;
-    const targetCurrency = document.getElementsByName("target-currency")[0].value;
-    let amount = parseFloat(document.getElementsByName("amount")[0].value);
-    if (isNaN(amount)) amount = 0.0;
+    const sourceCurrency = document.getElementById("dropdown1").value;
+    const targetCurrency = document.getElementById("dropdown2").value;
+    let amount = parseFloat(document.getElementById("amount").value);
+    if (isNaN(amount) || amount < 0) amount = 0.0;
 
     const URL = `${API_URL}/${API_KEY}/latest/${sourceCurrency}`;
-
     console.log(URL);
     convertCurrency(URL, targetCurrency, amount);
-
 });
 
 
 function extract(exchangeRates, targetCurrency, amount) {
     const value = exchangeRates[targetCurrency];
     const result = amount * value;
-    document.getElementById("result").innerText = "Converted value: " + result;
+    document.getElementById("result").innerText = `Converted value: ${targetCurrency}` + result;
 }
 
 const convertCurrency = (url, targetCurrency, amount) => {
